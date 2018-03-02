a21cd9f5c08f24f5cda273c029ca6874cef5467f
Larissa Varjão <larissavarjao@Macbook-Pro-Larissa.local>
2018-03-02 08:54:54 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,18 @@ app.post('/todos', (req, res) => {
       res.send(doc);
     },
     e => {
-      res.send(e);
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
     }
   );
 });
